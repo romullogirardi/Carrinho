0148d37f0d3ba92452d36bbf66a157a62331fc1f
girardi <romullogirardi@gmail.com>
2015-03-26 10:35:47 -0300

    Teste

--- src/com/romullogirardi/carrinho0main/model/GlobalReferences.java
+++ src/com/romullogirardi/carrinho0main/model/GlobalReferences.java
@@ -3,5 +3,5 @@ package com.romullogirardi.carrinho0main.model;
 import android.content.Context;
 
 public class GlobalReferences {
-	public static Context applicationContext; 
+	public static Context applicationContext;
 }
